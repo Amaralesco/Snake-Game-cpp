9e00f011d0baca68de6ad2a67cd2556000285786
Amaralesco <amaral.pap@gmail.com>
2023-12-27 18:50:58 +0000

    Setup Function

--- main.cpp
+++ main.cpp
@@ -1,10 +1,36 @@
 #include<iostream>
 using namespace std;
 bool gameOver;
+int score;
+
+//variable for map dimensions
+const int width = 20;
+const int height = 20;
+
+//Variables for Snake position
+int playerX;
+int playerY;
+
+int fruitX;
+int fruitY;
+
+//We need enumeration so player is not required to keep pressing the direction key
+enum eDirection {STOP =0, LEFT, RIGHT,UP,DOWN};
+eDirection dir;
 
 
 void Setup(){
     gameOver = false;
+    dir = STOP;
+    score = 0;
+
+    //center the player on the map
+    playerX = width / 2;
+    playerY = height / 2;
+
+    fruitX = rand() % width; 
+    fruitY = rand() % height;
+
 
 }
 
@@ -28,6 +54,7 @@ int main(){
         Draw();
         Input();
         Logic();
+        //Sleep(10);
     }
 
     return 0;   
