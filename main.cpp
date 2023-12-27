c729079a33c89796629e5401a54b2b6f9b5a1b43
Amaralesco <amaral.pap@gmail.com>
2023-12-27 20:10:30 +0000

    Working Game without Tail

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include <conio.h>
 using namespace std;
 bool gameOver;
 int score;
@@ -11,8 +12,11 @@ const int height = 20;
 int playerX;
 int playerY;
 
+//Variables for Fruit position
 int fruitX;
 int fruitY;
+
+
 
 //We need enumeration so player is not required to keep pressing the direction key
 enum eDirection {STOP =0, LEFT, RIGHT,UP,DOWN};
@@ -47,9 +51,18 @@ void Draw(){
             if (j==0){
                 cout << "#";
             }
+            
+            if( i == playerY & j == playerX) {
+                cout <<"O";
+            }
+            else if ( i == fruitY && j == fruitX){
+                cout << "F";
+            }
             else{
                 cout << " ";
             }
+
+            
 
             if (j == width -1 ){
                 cout << "#";
@@ -68,14 +81,72 @@ void Draw(){
     }
     cout << endl;
 
+    cout << "Score:" << score << endl;
+
 }
 
 void Input(){
+    //if keyboard hit true
+    if(_kbhit ()){
+        switch (_getch()){
+            case 'a':
+                dir = LEFT;
+                break;
+
+            case 'd':
+                dir = RIGHT;
+                break;
+
+            case 'w':
+                dir = UP;
+                break;
+
+            case 's':
+                dir = DOWN;
+                break;
+
+            case 'x':
+                gameOver = true;
+                break;
+        }
+    }
 
 }
 
 void Logic(){
 
+    switch (dir)
+    {
+    case LEFT:
+        playerX--; 
+        break;
+    
+    case RIGHT:
+        playerX++;
+        break;
+
+    case UP:
+        playerY--;
+        break;
+
+    case DOWN:
+        playerY++;
+        break;
+    
+    default:
+        break;
+    }
+    if(playerX > width || playerX <0 || playerY > height || playerY < 0){
+        gameOver = true;
+    }
+
+    if ( playerX == fruitX && playerY == fruitY){
+        score += 10;
+
+        //Spawn new fruit
+        fruitX = rand() % width; 
+        fruitY = rand() % height;   
+    }
 
 }
 
@@ -85,7 +156,8 @@ int main(){
         Draw();
         Input();
         Logic();
-        //Sleep(10);
+        //sleep(10);
+        _sleep(30);
     }
 
     return 0;   
